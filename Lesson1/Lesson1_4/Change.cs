3f6cba0712f5a3337122cea09dc4320ea54af56e
solodukho <solodukho@bk.ru>
2020-02-20 15:40:34 +0100

    Домашнее задание к первому уроку

---
+++ Lesson1/Lesson1_4/Change.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson1_4
+{
+    class Change
+    {
+        /* [Sergej Solodukho]
+          * 4. Написать программу обмена значениями двух переменных.
+             а) с использованием третьей переменной;
+             б) *без использования третьей переменной.
+          */
+        static void Main(string[] args)
+        {
+            Console.Write("Введите значение первой переменной (x): ");
+            var x = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите значение второй переменной (y): ");
+            var y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Вы ввели: x = {x}, y = {y}");
+
+            // а) с использованием третьей переменной;
+            //var temp = x;
+            //x = y;
+            //y = temp;
+
+            // б) *без использования третьей переменной.
+            //x += y - (y = x);
+            // или
+            x ^= y ^ (y = x);
+
+            Console.WriteLine($"Результат обмена значениями: x = {x}, y = {y}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lesson1/Lesson1_5/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Lesson1_5
+{
+    class Program
+    {
+        /* [Solodukho]
+        * 5.
+           а) Написать программу, которая выводит на экран ваше имя, фамилию и город проживания.
+           б) Сделать задание, только вывод организуйте в центре экрана
+           в) *Сделать задание б с использованием собственных методов (например, Print(string ms, int x,int y)
+       */
+        static void Main(string[] args)
+        {
+            string output = "Bill Gates. Seattle";
+
+            // а)
+            //Console.WriteLine($"{name} {surname}. {city}");
+            //Console.ReadKey();
+
+            // б) Сделать задание, только вывод организуйте в центре экрана      
+            //Console.SetCursorPosition((Console.WindowWidth - output.Length) / 2, Console.WindowHeight / 2);
+            //Console.WriteLine(output);
+            //Console.ReadKey();
+
+            // в) *Сделать задание б с использованием собственных методов (например, Print(string ms, int x,int y)
+            Print(output, (Console.WindowWidth - output.Length) / 2, Console.WindowHeight / 2);
+            Console.ReadKey();
+        }
+
+        private static void Print(string ms, int x, int y)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.WriteLine(ms);
+        }
+    }
+}
