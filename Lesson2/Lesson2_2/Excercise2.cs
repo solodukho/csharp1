2a5d08b603609bd55349d95e3ba1add6098df5d7
solodukho <solodukho@bk.ru>
2020-02-25 18:09:23 +0100

    Д.З. ко второму уроку

---
+++ Lesson2/Lesson2_2/Excercise2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Lesson2_2
+{
+    /* [Sergej Solodukho] 
+     * 2.	Написать метод подсчета количества цифр числа. */
+    class Excercise2 
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число: ");
+            string input = Console.ReadLine();
+            Console.WriteLine($"Количество цифр в этом числе = {input.Length}");
+
+            Console.ReadKey();
+        }
+        
+    }
+}
